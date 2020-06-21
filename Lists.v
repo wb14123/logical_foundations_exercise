383b25c3524f1cb2961823651c9c582356bb33fd
Bin Wang <bin.wang@mail.binwang.me>
2020-06-20 20:35:35 -0400

    eqblist_exercise

--- Lists.v
+++ Lists.v
@@ -922,25 +922,36 @@ Qed.
     lists of numbers for equality.  Prove that [eqblist l l]
     yields [true] for every list [l]. *)
 
-Fixpoint eqblist (l1 l2 : natlist) : bool
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Fixpoint eqblist (l1 l2 : natlist) : bool :=
+  match l1, l2 with
+  | nil, nil => true
+  | nil, _ => false
+  | _, nil => false
+  | h1::t1, h2::t2 => match h1 =? h2 with
+    | true => eqblist t1 t2
+    | false => false
+    end
+  end.
 
 Example test_eqblist1 :
   (eqblist nil nil = true).
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_eqblist2 :
   eqblist [1;2;3] [1;2;3] = true.
-(* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_eqblist3 :
   eqblist [1;2;3] [1;2;4] = false.
- (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Theorem eqblist_refl : forall l:natlist,
   true = eqblist l l.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  induction l as [| n l' Hl'].
+  - reflexivity.
+  - simpl. rewrite -> self_eq. rewrite <- Hl'. reflexivity.
+Qed.
 (** [] *)
 
 (* ================================================================= *)
