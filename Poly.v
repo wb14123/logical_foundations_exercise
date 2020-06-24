df64bef3332100c6d8953b316d6b791ba583fc92
Bin Wang <bin.wang@mail.binwang.me>
2020-06-24 17:23:06 -0400

    fold_map exercise

--- Poly.v
+++ Poly.v
@@ -1028,15 +1028,23 @@ Qed.
     We can also define [map] in terms of [fold].  Finish [fold_map]
     below. *)
 
-Definition fold_map {X Y: Type} (f: X -> Y) (l: list X) : list Y
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Definition fold_map {X Y: Type} (f: X -> Y) (l: list X) : list Y :=
+  fold (fun x result => (f x) :: result) l [].
 
 (** Write down a theorem [fold_map_correct] in Coq stating that
    [fold_map] is correct, and prove it.  (Hint: again, remember that
    [reflexivity] simplifies expressions a bit more aggressively than
    [simpl].) *)
 
-(* FILL IN HERE *)
+Theorem fold_map_correct :
+  forall X Y (l : list X) (f: X -> Y),
+  fold_map f l = map f l.
+Proof.
+intros X Y l f.
+induction l as [| n l' Hl'].
+- reflexivity.
+- simpl. rewrite <- Hl'. reflexivity.
+Qed.
 
 (* Do not modify the following line: *)
 Definition manual_grade_for_fold_map : option (nat*string) := None.
