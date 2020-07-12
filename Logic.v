49a594cd66dfa47ed09136412c7f087515dd96fd
Bin Wang <bin.wang@mail.binwang.me>
2020-07-12 13:16:51 -0400

    and_assoc exercise

--- Logic.v
+++ Logic.v
@@ -267,7 +267,13 @@ Theorem and_assoc : forall P Q R : Prop,
   P /\ (Q /\ R) -> (P /\ Q) /\ R.
 Proof.
   intros P Q R [HP [HQ HR]].
-  (* FILL IN HERE *) Admitted.
+  split.
+  - split.
+    + apply HP.
+    + apply HQ.
+  - apply HR.
+Qed.
+
 (** [] *)
 
 (** By the way, the infix notation [/\] is actually just syntactic
